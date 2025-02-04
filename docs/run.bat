30772140d11d1e454acbd41fb7d1149ad0eddb5f
Maclain Wiltzer <mwiltzer@gmail.com>
2025-02-04 06:31:54 -0500

    Delete docs directory
    I don't need docs

--- docs/run.bat
+++
@@ -1,6 +0,0 @@
-del _site /s /q
-
-rem Documentation spell check
-powershell -ExecutionPolicy ByPass -NoProfile -File "..\build.ps1" --target SpellCheck --no-logo
-
-bundle exec jekyll serve --incremental
